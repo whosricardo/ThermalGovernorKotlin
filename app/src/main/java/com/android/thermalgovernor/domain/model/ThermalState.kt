3bdee342aba5266f18b4727fecf74eec10221e04
Ricardo <ricardo.sergio.pff@gmail.com>
2026-07-28 10:17:36 -0300

    refactor to abstract properties

--- app/src/main/java/com/android/thermalgovernor/domain/model/ThermalState.kt
+++ app/src/main/java/com/android/thermalgovernor/domain/model/ThermalState.kt
@@ -2,23 +2,31 @@ package com.android.thermalgovernor.domain.model
 
 sealed class ThermalState {
 
-    val tempCelsius: Float    
-    val message: String
+    abstract val tempCelsius: Float    
+    abstract val warningMessage: String
 
     // data class (needs unique value)
-    data class Cool() : ThermalState() {
-        val warningMessage = "SoC is cool ($tempCelsius°C). Nothing to be done."
+    data class Cool(
+        override val tempCelsius: Float
+    ) : ThermalState() {
+        override val warningMessage = "SoC is cool ($tempCelsius°C). Nothing to be done."
     }
     
-    data class Warm() : ThermalState() {
-        val warningMessage = "SoC is warming up ($tempCelsius°C). Deferring non-essential tasks."
+    data class Warm(
+        override val tempCelsius: Float
+    ) : ThermalState() {
+        override val warningMessage = "SoC is warming up ($tempCelsius°C). Deferring non-essential tasks."
     }
 
-    data class Hot() : ThermalState() {
-        val warningMessage = "SoC is HOT ($tempCelsius°C)! Restricting background CPU threads."
+    data class Hot(
+        override val tempCelsius: Float
+    ) : ThermalState() {
+        override val warningMessage = "SoC is HOT ($tempCelsius°C)! Restricting background CPU threads."
     }
     
-    data class Critical() : ThermalState() {
-        val warningMessage = "CRITICAL THERMAL LEVEL ($tempCelsius°C)! Initiating system mitigation."
+    data class Critical(
+        override val tempCelsius: Float
+    ) : ThermalState() {
+        override val warningMessage = "CRITICAL THERMAL LEVEL ($tempCelsius°C)! Initiating system mitigation."
     }
 }
