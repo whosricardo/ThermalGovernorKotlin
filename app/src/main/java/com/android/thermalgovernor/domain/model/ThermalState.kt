f94e9443ff2a20323a1d922c7dc72beb4dafc6bf
Ricardo <ricardo.sergio.pff@gmail.com>
2026-07-28 10:09:38 -0300

    elevated properties to parent class

--- app/src/main/java/com/android/thermalgovernor/domain/model/ThermalState.kt
+++ app/src/main/java/com/android/thermalgovernor/domain/model/ThermalState.kt
@@ -1,21 +1,24 @@
 package com.android.thermalgovernor.domain.model
 
 sealed class ThermalState {
-    
+
+    val tempCelsius: Float    
+    val message: String
+
     // data class (needs unique value)
-    data class Cool(val tempCelsius: Float) : ThermalState() {
+    data class Cool() : ThermalState() {
         val warningMessage = "SoC is cool ($tempCelsius°C). Nothing to be done."
     }
     
-    data class Warm(val tempCelsius: Float) : ThermalState() {
+    data class Warm() : ThermalState() {
         val warningMessage = "SoC is warming up ($tempCelsius°C). Deferring non-essential tasks."
     }
 
-    data class Hot(val tempCelsius: Float) : ThermalState() {
+    data class Hot() : ThermalState() {
         val warningMessage = "SoC is HOT ($tempCelsius°C)! Restricting background CPU threads."
     }
     
-    data class Critical(val tempCelsius: Float) : ThermalState() {
+    data class Critical() : ThermalState() {
         val warningMessage = "CRITICAL THERMAL LEVEL ($tempCelsius°C)! Initiating system mitigation."
     }
 }
