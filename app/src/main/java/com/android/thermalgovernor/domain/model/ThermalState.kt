5404cef4a2252218b51abd21a58eba813c4fef5d
Ricardo <ricardo.sergio.pff@gmail.com>
2026-07-28 10:03:26 -0300

    changed warning messages on thermalState

--- app/src/main/java/com/android/thermalgovernor/domain/model/ThermalState.kt
+++ app/src/main/java/com/android/thermalgovernor/domain/model/ThermalState.kt
@@ -3,19 +3,19 @@ package com.android.thermalgovernor.domain.model
 sealed class ThermalState {
     
     // data class (needs unique value)
-    data class Cool(val temp: Float) : ThermalState {
+    data class Cool(val tempCelsius: Float) : ThermalState() {
         val message = "not developed"
     }
     
-    data class Warm(val temp: Float) : ThermalState {
-        val message = "not developed"
+    data class Warm(val tempCelsius: Float) : ThermalState() {
+        val warningMessage = "SoC is warming up ($tempCelsius°C). Deferring non-essential tasks."
     }
 
-    data class Hot(val temp: Float) : ThermalState {
-        val message = "not developed"
+    data class Hot(val tempCelsius: Float) : ThermalState() {
+        val warningMessage = "SoC is HOT ($tempCelsius°C)! Restricting background CPU threads."
     }
     
-    data class Critical(val temp: Float) : ThermalState {
-        val message = "not developed"
+    data class Critical(val tempCelsius: Float) : ThermalState() {
+        val warningMessage = "CRITICAL THERMAL LEVEL ($tempCelsius°C)! Initiating system mitigation."
     }
 }
