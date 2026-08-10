22745e07f1baa1f17be3991e52612fd1bb5db7b6
Ricardo <ricardo.sergio.pff@gmail.com>
2026-08-10 17:50:22 -0300

    added thermal repository and implemented method to get cpu temperatures

--- app/src/main/java/com/android/thermalgovernor/data/ThermalRepository.kt
+++ app/src/main/java/com/android/thermalgovernor/data/ThermalRepository.kt
@@ -1,1 +1,14 @@
 package com.android.thermalgovernor.data
+
+import android.os.HardwarePropertiesManager
+
+class ThermalRepository(
+    private val hardwarePropertiesManager: HardwarePropertiesManager
+) {
+    fun getCpuTemperatures(): FloatArray {
+        return hardwarePropertiesManager.getDeviceTemperatures(
+            hardwarePropertiesManager.DEVICE_TEMPERATURE_CPU,
+            hardwarePropertiesManager.TEMPERATURE_CURRENT
+        )
+    }
+}
