d67164b77bc1af7bbb8fd2ac26002572ee12872e
Ricardo <ricardo.sergio.pff@gmail.com>
2026-08-11 17:29:55 -0300

    implemented basic logic and activity creation, created tag

---
+++ app/src/main/java/com/android/thermalgovernor/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.thermalgovernor
+
+import androidx.appcompact.app.AppCompactActivity
+import android.os.Bundle
+
+class MainActivity: AppCompactActivity() {
+    companion object {
+        private const val TAG = "ThermalGovernor"
+    }
+
+    override fun onCreate(savedInstaceState: Bundle?) {
+        super.onCreate(savedInstaceState)
+    }
+}
